797bb7d6c44c850ea26d7d6f310b07d330d7508a
samsonc <samsonc0514@gmail.com>
2023-05-26 12:38:57 +0800

    added blog page and modified blog box(home)

--- lba-blog/app/(site)/page.tsx
+++ lba-blog/app/(site)/page.tsx
@@ -1,12 +1,13 @@
 import { getProjects } from "@/sanity/sanity-utils";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home() {
   //get props and paths the project and map the data to the page
   const projects = await getProjects();
 
   return (
-    <div className="max-w-5xl mx-auto py-20">
+    <div>
       <h1 className="text-7xl font-extrabold">
         Hello this is{" "}
         <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
@@ -22,23 +23,28 @@ export default async function Home() {
 
       <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {projects.map((project) => (
-          <div
+          <Link
+            href={`/projects/${project.slug}`}
             key={project._id}
-            className="border border-gray-500 rounded-lg p-3"
+            className="border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition"
           >
             {project.image && (
               <Image
                 src={project.image}
                 alt={project.name}
-                width={250}
-                height={100}
+                width={750}
+                height={300}
                 className="object-cover rounded-lg border border-gray-500"
               />
             )}
-            <div className="font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
-              {project.name}{" "}
+            <header className="flex items-center justify-between">
+              <div className="mt-2 mx-2 font-extralight">{project.date}</div>
+              <div className="mt-2 mx-2 font-extralight">{project.author} </div>
+            </header>
+            <div className="mx-2 text-2xl font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
+              {project.name}
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
--- lba-blog/types/Project.ts
+++ lba-blog/types/Project.ts
@@ -1,11 +1,13 @@
 import { PortableTextBlock } from "sanity";
 
 export type Project = {
-    _id: string;
-    _createAt: Date;
-    name: string;
-    slug: string;
-    image: string;
-    url: string;
-    content: PortableTextBlock[];
-}
+  _id: string;
+  _createAt: Date;
+  name: string;
+  author: string;
+  date: string;
+  slug: string;
+  image: string;
+  url: string;
+  content: PortableTextBlock[];
+};
