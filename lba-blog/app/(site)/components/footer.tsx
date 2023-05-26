01c87b471e3412580c3619d629ee9e10b14c1ae4
DWKim67 <dkim67@my.yorku.ca>
2023-05-26 11:12:26 -0400

    Created footer, added author + date in blog pages
    Run "npm install moment --save" for date formatting

---
+++ lba-blog/app/(site)/components/footer.tsx
@@ -0,0 +1,30 @@
+
+export default function Footer() {
+    return (
+        <div className="flex">
+            <div className="flex-none mr-10">
+                <h1 className='font-bold'>Pages</h1>
+                <div className="mt-5">
+                    <h2 className="mt-3">NFT</h2>
+                    <h2 className="mt-3">Team</h2>
+                    <h2 className="mt-3">Blog</h2>
+                    <h2 className="mt-3">Demo</h2>
+                </div>
+            </div>
+            <div className="flex-none mr-10">
+                <h1 className='font-bold'>About</h1>
+                <div className="mt-4">
+                    <h2 className="mt-3">Blog</h2>
+                    <h2 className="mt-3">Main Website</h2>
+                    <h2 className="mt-3">UI/UX Design</h2>
+                </div>
+            </div>
+            <div className="flex-none mr-10">
+                <h1 className='font-bold'>Contact</h1>
+                <div className="mt-5">
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
