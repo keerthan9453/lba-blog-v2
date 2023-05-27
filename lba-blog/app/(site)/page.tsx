179aa4c6ee5d1888a5ca3c8c35a7778425831e62
samsonc <samsonc0514@gmail.com>
2023-05-27 18:06:42 +0800

    updated blog sanity schema

--- lba-blog/app/(site)/page.tsx
+++ lba-blog/app/(site)/page.tsx
@@ -1,54 +1,80 @@
-import { getProjects } from "@/sanity/sanity-utils";
+import { getBlogs } from "@/sanity/sanity-utils";
 import Image from "next/image";
 import Link from "next/link";
 import Footer from "./components/footer";
+import moment from "moment";
 
 export default async function Home() {
-  //get props and paths the project and map the data to the page
-  const projects = await getProjects();
+  //get props and paths the blog and map the data to the page
+  const blogs = await getBlogs();
 
-  return (<>
-    <div>
-      <h1 className="text-7xl font-extrabold">
-        Hello this is{" "}
-        <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
-          Lassonde Blockchain
-        </span>
-      </h1>
+  return (
+    <>
+      <div>
+        {/* header section */}
+        <h1 className="text-7xl font-extrabold">
+          Hello this is{" "}
+          <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
+            Lassonde Blockchain
+          </span>
+        </h1>
 
-      <p className="mt-3 text-xl text-gray-600">
-        Weclome everyone! Check out our blogs!
-      </p>
+        <p className="mt-3 text-xl text-gray-600">
+          Weclome everyone! Check out our blogs!
+        </p>
 
-      <h2 className="mt-24 font-bold text-gray-700 text-3xl">Blogs</h2>
+        <h2 className="mt-24 font-bold text-gray-700 text-3xl">Blogs</h2>
 
-      <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {projects.map((project) => (
-          <Link
-            href={`/projects/${project.slug}`}
-            key={project._id}
-            className="border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition"
-          >
-            {project.image && (
-              <Image
-                src={project.image}
-                alt={project.name}
-                width={750}
-                height={300}
-                className="object-cover rounded-lg border border-gray-500"
-              />
-            )}
-            <header className="flex items-center justify-between">
-              <div className="mt-2 mx-2 font-extralight">{project.date}</div>
-              <div className="mt-2 mx-2 font-extralight">{project.author} </div>
-            </header>
-            <div className="mx-2 text-2xl font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
-              {project.name}
-            </div>
-          </Link>
-        ))}
+        {/* blog box section*/}
+        <div className="mt-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 md:p-6">
+          {/* display info from each blog */}
+          {blogs.map((blog) => (
+            <Link
+              href={`/projects/${blog.slug}`}
+              key={blog._id}
+              // className="border-2 border-gray-500 rounded-lg p-1 "
+            >
+              <div className=" border rounded-lg group cursor-pointer overflow-hidden">
+                {blog.image && (
+                  <Image
+                    src={blog.image}
+                    alt={blog.title}
+                    width={750}
+                    height={300}
+                    // className="object-cover rounded-lg border border-gray-500"
+                    className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                  />
+                )}
+                <header className="flex items-center justify-between">
+                  <div className="mt-2 mx-2 font-extralight">
+                    {moment(blog.publichedAt).format("MMM Do, YYYY")}
+                  </div>
+                  <div className="mt-2 mx-2 font-extralight">
+                    {blog.author.name}{" "}
+                  </div>
+                </header>
+                <header className="flex items-center justify-between">
+                  <div className="mx-2 text-2xl font-extrabold">
+                    {blog.title}
+                  </div>
+                  {blog.author.image && (
+                    <Image
+                      src={blog.author.image}
+                      alt={blog.author.name}
+                      className="mx-2 h-12 w-12 counded-full"
+                    />
+                  )}
+                </header>
+                <p className="mx-2 text-xl font-extralight">
+                  {blog.description}
+                </p>
+              </div>
+            </Link>
+          ))}
+        </div>
+        {/* footer section */}
+        <Footer />
       </div>
-    </div>
-    <Footer></Footer>
-  </>);
+    </>
+  );
 }
--- lba-blog/sanity/sanity-utils.ts
+++ lba-blog/sanity/sanity-utils.ts
@@ -1,33 +1,38 @@
-import { Project } from "@/types/Project";
+import { Blog } from "@/types/Blog";
 import { createClient, groq } from "next-sanity";
 import clientConfig from "./config/client-config";
 
-export async function getProjects(): Promise<Project[]> {
+// fetching the data from sanity
+//  data for home page
+export async function getBlogs(): Promise<Blog[]> {
   return createClient(clientConfig).fetch(
     groq`*[_type == "project"]{
             _id,
             _createdAt,
-            name,
-            author,
-            date,
+            title,
+            description,
+            author -> {name, image},
+            publichedAt,
             "slug": slug.current,
             "image": image.asset->url,
-            url,
+            categories,
             content
         }`
   );
 }
-export async function getProject(slug: string): Promise<Project> {
+//  data for individual blog page
+export async function getBlog(slug: string): Promise<Blog> {
   return createClient(clientConfig).fetch(
     groq`*[_type == "project" && slug.current == $slug][0]{
             _id,
             _createdAt,
-            name,
-            author,
-            date, 
+            title,
+            description,
+            author -> {name, image},
+            publichedAt, 
             "slug": slug.current,
             "image": image.asset->url,
-            url,
+            categories,
             content
         }`,
     { slug }
