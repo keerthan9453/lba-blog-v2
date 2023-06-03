7e574f100c9893796fe63d52ae1fc6cccf9d0297
DWKim67 <dkim67@my.yorku.ca>
2023-06-03 11:13:23 -0400

    Refactored page.tsx to separate trending blogs into separate component

--- lba-blog/app/(site)/page.tsx
+++ lba-blog/app/(site)/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import moment from "moment";
 import FeaturedSidebar from "./components/FeaturedSidebar";
 import CategoryTab from "./components/CategoryTab";
+import TrendingBlogs from "./components/TrendingBlogs";
 
 export default async function Home() {
   //get props and paths the blog and map the data to the page
@@ -36,51 +37,7 @@ export default async function Home() {
         <div className="mt-5 grid grid-cols-4">
           <div className="col-span-3">
             {/* display info from each blog */}
-            {blogs.map((blog) => (
-              <Link
-                href={`/projects/${blog.slug}`}
-                key={blog._id}
-                // className="border-2 border-gray-500 rounded-lg p-1 "
-              >
-                <div className="shadow-lg flex items-center my-5 mr-3">
-                  <div className=" group cursor-pointer overflow-hidden p-5 ">
-                    {blog.image && (
-                      <Image
-                        src={blog.image}
-                        alt={blog.title}
-                        width={200}
-                        height={200}
-                        // className="object-cover rounded-lg border border-gray-500"
-                        className=" flex h-60 w-60 object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
-                      />
-                    )}
-                  </div>
-                  <div className="flex flex-col px-4">
-                    <p className=" text-sm tracking-wider uppercase">
-                      Categories: {blog.categories.toString()}
-                    </p>
-                    <p>{blog.categories.description}</p>
-                    <p className="mt-8">
-                      {moment(blog.publichedAt).format("MMM D")}
-                    </p>
-                    <p className="text-5xl font-extrabold">{blog.title}</p>
-                    {/* {blog.author && (
-                      <Image
-                        src={blog.author.image}
-                        alt={blog.author.name}
-                        className="mx-2 h-12 w-12 counded-full"
-                      />
-                    )} */}
-                    <p className="mt-1 text-sm uppercase font-extrabold">
-                      {blog.description}
-                    </p>
-                    <span className="uppercase font-extralight">
-                      By {blog.author.name}{" "}
-                    </span>
-                  </div>
-                </div>
-              </Link>
-            ))}
+            <TrendingBlogs blogs={blogs} />
           </div>
           <FeaturedSidebar />
         </div>
