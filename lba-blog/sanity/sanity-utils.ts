e7f6976481f8be5de9b9d95ff051f4368a5f3801
samsonc <samsonc0514@gmail.com>
2023-06-06 23:19:24 +0800

    added groq filter for each category

--- lba-blog/sanity/sanity-utils.ts
+++ lba-blog/sanity/sanity-utils.ts
@@ -56,9 +56,46 @@ export async function getCategories(): Promise<Category[]> {
     }[defined(id)]`
   );
 }
-export async function getFilterCategoryBlogs(): Promise<Category[]> {
+// Blockchain Blog Filter
+export async function getFilterBlockchainBlogs(): Promise<Blog[]> {
   return createClient(clientConfig).fetch(
-    groq`*[_type == 'category' && slug in categories[]->slug.current]{
+    groq`*[_type == 'project' && categories[]->title match "Blockchain"]{
+      _id,
+      _createdAt,
+      title,
+      description,
+      author -> {name, "authorImage": image.asset->url},
+      
+      // loop through all the entries in categories and return the title from the referenced document
+      "categories": categories[]->title, 
+      publichedAt,
+      "slug": slug.current,
+      "image": image.asset->url,
+      content}`
+  );
+}
+// AI/ML Blog Filter
+export async function getFilterAIBlogs(): Promise<Blog[]> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == 'project' && categories[]->title match "AI/ML"]{
+      _id,
+      _createdAt,
+      title,
+      description,
+      author -> {name, "authorImage": image.asset->url},
+      
+      // loop through all the entries in categories and return the title from the referenced document
+      "categories": categories[]->title, 
+      publichedAt,
+      "slug": slug.current,
+      "image": image.asset->url,
+      content}`
+  );
+}
+// Metaverse Blog Filter
+export async function getFilterMetaverseBlogs(): Promise<Blog[]> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == 'project' && categories[]->title match "Metaverse"]{
       _id,
       _createdAt,
       title,
