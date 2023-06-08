bc3a96102e78447ce58d8bdb0a71695ee09d24c6
Nikhil <nikhilsharma26500@outlook.com>
2023-06-08 20:01:26 +0530

    dark mode buttons

--- lba-blog/app/(site)/components/ThemeSwitcher.tsx
+++ lba-blog/app/(site)/components/ThemeSwitcher.tsx
@@ -2,7 +2,9 @@
 
 import { useTheme } from "next-themes";
 import { useState, useEffect } from "react";
-import * as Switch from '@radix-ui/react-switch';
+// import * as Switch from '@radix-ui/react-switch';
+import { MdDarkMode } from 'react-icons/md';
+import { FaSun } from 'react-icons/fa';
 
 const ThemeSwitcher = () => {
     const [mounted, setMounted] = useState(false);
@@ -15,7 +17,9 @@ const ThemeSwitcher = () => {
     }
 
     return (
-        // <div className="flex items-center" style={{ display: 'flex', alignItems: 'center' }}>
+        <>
+
+            {/* // <div className="flex items-center" style={{ display: 'flex', alignItems: 'center' }}>
         //     <label className="text-white text-[15px] leading-none pr-[15px]" htmlFor="airplane-mode">
         //         Light/Dark Mode
         //     </label>
@@ -25,12 +29,23 @@ const ThemeSwitcher = () => {
         //     >
         //         <Switch.Thumb className="block w-[21px] h-[21px] bg-white rounded-full shadow-[0_2px_2px] shadow-blackA7 transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]" />
         //     </Switch.Root>
-        // </div>
-        <div>
-            The current theme is: {theme}
-            <button onClick={() => setTheme("light")}>Light Mode</button>
-            <button onClick={() => setTheme("dark")}>Dark Mode</button>
-        </div>
+        // </div> */}
+
+            <div className="fixed right-10 duration-100 dark:bg-slate-800 bg-grap-100 rounded">
+                {/* light button */}
+                <button
+                    onClick={() => setTheme("light")}
+                    className="w-8 h-8 leading-9 text-x1 rounded-full m-1 text-sky-600">
+                    <FaSun className="mx-2" target="_blank" />
+                </button>
+                {/* dark button */}
+                <button
+                    onClick={() => setTheme("dark")}
+                    className="w-8 h-8 leading-9 text-x1 rounded-full m-1 text-sky-600">
+                    <MdDarkMode className="mx-2" target="_blank" />
+                </button>
+            </div>
+        </>
     )
 };
 
