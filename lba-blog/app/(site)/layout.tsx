84e40334861203a238285e7b3b066013c996b86e
samsoncn <42096701+samsoncn@users.noreply.github.com>
2023-06-08 10:58:46 -0400

    Update layout.tsx

--- lba-blog/app/(site)/layout.tsx
+++ lba-blog/app/(site)/layout.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import "../globals.css";
 import { Inter } from "next/font/google";
-import Footer from "./components/Footer";
+import Footer from "./components/footer";
 import CategoryTab from "./components/CategoryTab";
 
 const inter = Inter({ subsets: ["latin"] });
