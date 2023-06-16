c54bd82f44005f574d57025cde0ae22f2788bdd7
Nikhil Sharma <nikhilsharma26500@outlook.com>
2023-06-16 22:59:50 +0530

    Update TrendingBlogs

--- lba-blog/app/(site)/components/TrendingBlogs.tsx
+++ lba-blog/app/(site)/components/TrendingBlogs.tsx
@@ -21,9 +21,8 @@ function TrendingBlogs({ inputBlogs }: TrendingBlogProps) {
         <Link
           href={`/projects/${blog.slug}`}
           key={blog._id}
-          // className="border-2 border-gray-500 rounded-lg p-1 "
         >
-          <div className="shadow-lg rounded dark:shadow-slate-600 dark:bg-[#1e1e1e] flex flex-col md:flex-row items-center my-5 mr-3">
+          <div className="shadow-lg rounded dark:shadow-slate-600 dark:bg-[#1e1e1e] flex flex-col md:flex-row items-center my-5 mx-3">
             <div className="group cursor-pointer overflow-hidden p-5">
               {blog.image && (
                 <Image
@@ -31,7 +30,6 @@ function TrendingBlogs({ inputBlogs }: TrendingBlogProps) {
                   alt={blog.title}
                   width={200}
                   height={200}
-                  // className="object-cover rounded-lg border border-gray-500"
                   className="flex rounded h-60 w-60 object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
                 />
               )}
@@ -47,17 +45,10 @@ function TrendingBlogs({ inputBlogs }: TrendingBlogProps) {
               <p className="text-2xl md:text-5xl font-extrabold">
                 {blog.title}
               </p>
-              {/* {blog.author && (
-                <Image
-                  src={blog.author.image}
-                  alt={blog.author.name}
-                  className="mx-2 h-12 w-12 counded-full"
-                />
-              )} */}
               <p className="mt-1 text-sm uppercase font-extrabold">
                 {blog.description}
               </p>
-              <span className="uppercase font-extralight">
+              <span className="uppercase font-extralight pb-5">
                 By {blog.author.name}
               </span>
             </div>
