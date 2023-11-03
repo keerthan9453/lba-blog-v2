8e033fe28ec1f77833b432f808d7c27221ff030d
Zauce <122509414+Zee-25@users.noreply.github.com>
2023-11-02 23:04:52 -0400

    Added Rough Login Page

---
+++ app/(site)/login-page/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React, { useState } from "react";
+
+const Login = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [emailError, setEmailError] = useState("")
+    const [passwordError, setPasswordError] = useState("")
+    
+        
+    const onButtonClick = () => {
+    }
+
+    return <div className={"flex flex-col items-center justify-center h-screen"} >
+        <div className={"flex flex-col text-4xl font-bold items-center justify-center"}>
+            <div>Login</div>
+        </div>
+        <br />
+        <div className={"flex flex-col items-start justify-center"}>
+            <input
+                value={email}
+                placeholder="Enter your email here"
+                onChange={ev => setEmail(ev.target.value)}
+                className={"h-12 w-96 text-lg rounded-md border border-gray-300 pl-2"} />
+            <label className="text-red-500 text-sm">{emailError}</label>
+        </div>
+        <br />
+        <div className={"flex flex-col items-start justify-center"}>
+            <input
+                value={password}
+                placeholder="Enter your password here"
+                onChange={ev => setPassword(ev.target.value)}
+                className={"h-12 w-96 text-lg rounded-md border border-gray-300 pl-2"} />
+            <label className="text-red-500 text-sm">{passwordError}</label>
+        </div>
+        <br />
+        <div className={"flex flex-col items-start justify-center"}>
+            <input
+                className={"inputButton"}
+                type="button"
+                onClick={onButtonClick}
+                value={"Log in"} />
+        </div>
+    </div>
+}
+
+export default Login
