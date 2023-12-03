31fb74661e170605ff871a4ec34dd6d03984dac9
Humza987 <humzainam@gmail.com>
2023-12-02 20:18:24 -0500

    UI changes to Blog Form

--- app/(site)/submit-form/page.tsx
+++ app/(site)/submit-form/page.tsx
@@ -90,8 +90,8 @@ function MyForm() {
 
   const isTitleValid = wordCount(formData.title) <= 25;
   const isDescriptionValid = wordCount(formData.description) <= 100;
-<!--     if (file) reader.readAsDataURL(file);
-  }, [file]); -->
+//     if (file) reader.readAsDataURL(file);
+//  }, [file]); 
   // const reader = new FileReader();
   // reader.onload = (event) => {
   //   if (event.target) {
@@ -106,7 +106,7 @@ function MyForm() {
     <form onSubmit={handleSubmit}>
       <div className="mt-28 flex align-items justify-center mb-28 w-2/3 flex-col mx-auto">
         <div className="mx-auto text-4xl">Post a Blog</div>
-        <div className="mt-4">
+        {/* <div className="mt-4">
           <label htmlFor="author">Author:</label>
           <input
             type="text"
@@ -114,70 +114,38 @@ function MyForm() {
             name="author"
             value={formData.author}
             onChange={handleInputChange}
-            className="border w-full rounded py-2 text-white leading-tight bg-transparent focus:outline-none focus:shadow-outline"
+            className="border w-full rounded-xl py-2 text-white leading-tight bg-slate-800 focus:outline-none focus:shadow-outline"
           />
-        </div>
-        <div className="mt-4">
-          <label htmlFor="title">Title:</label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            value={formData.title}
-            onChange={handleInputChange}
-            className={`border w-full rounded py-2 text-white-700 leading-tight ${
-              isTitleValid ? "bg-transparent" : "bg-red-200"
-            } focus:outline-none focus:shadow-outline`}
-<!--              className="border w-full rounded py-2 text-white leading-tight bg-transparent focus:outline-none focus:shadow-outline" -->
-          />
-          <p className="text-sm text-gray-500">
-            {wordCount(formData.title)} / 25 words
-          </p>
-        </div>
-        <div className="mt-4">
-          <label htmlFor="slug">Slug:</label>
-          <input
-            type="text"
-            id="slug"
-            name="slug"
-            value={formData.slug}
-            readOnly
-            className="border w-full rounded py-2 text-white-700 leading-tight bg-transparent focus:outline-none focus:shadow-outline"
-<!--             onChange={handleInputChange}
-            className="border w-full rounded py-2 text-white leading-tight bg-transparent focus:outline-none focus:shadow-outline" -->
-          />
-        </div>
-        <div className="mt-4">
-          <label htmlFor="description" className="mb-2">
-            Description:
-          </label>
-          <div>
-            <textarea
-              id="description"
-              name="description"
-              value={formData.description}
-              onChange={handleInputChange}
-              className={`border w-full rounded py-2 text-white-700 leading-tight ${
-                isDescriptionValid ? 'bg-transparent' : 'bg-black-700'
-              } focus:outline-none focus:shadow-outline`}
-<!--               className="border  w-full rounded py-2 text-white leading-tight bg-transparent focus:outline-none focus:shadow-outline" -->
-            />
+        </div> */}
+        <div className="flex flex-row justify-between">
+            <div className="mt-4 flex-grow">
+            {/* <label htmlFor="date" className="mb-2">
+              Date:
+            </label> */}
+            {/* <div className="flex-1"> */}
+              <input
+                type="text"
+                onFocus={(e) => (e.target.type = "date")}
+                onBlur={(e) => (e.target.type = "text")}                
+                id="date"
+                name="date"
+                value={formData.date}
+                onChange={handleInputChange}
+                placeholder="Date"
+                className="border w-full rounded-xl pl-[7px] py-2 text-white-700 leading-tight bg-slate-800 focus:outline-none focus:shadow-outline"
+              />
+            {/* </div> */}
           </div>
-          <p className="text-sm text-gray-500">
-          {wordCount(formData.description)} / 100 words
-          </p>
-        </div>
-        <div className="mt-4">
-          <label htmlFor="category">Category:</label>
+         <div className="mt-4 flex-grow ml-2">
           <select
             id="category"
             name="category"
             value={formData.category}
             onChange={handleInputChange}
-            className="border w-full rounded py-2 text-white-700 leading-tight bg-transparent focus:outline-none focus:shadow-outline"
+            className="border w-full rounded-xl pl-[7px] py-2 text-white-700 leading-tight bg-slate-800 focus:outline-none focus:shadow-outline"
           >
             <option value="" disabled style={{ color: "white" }}>
-              Select a category
+              Category
             </option>
             {categories.map((category, index) => (
               <option key={index} value={category} style={{ color: "black" }}>
@@ -186,36 +154,76 @@ function MyForm() {
             ))}
           </select>
         </div>
+        </div>
+
         <div>
-          <div className="mt-4">
-            <label htmlFor="date" className="mb-2">
-              Date:
-            </label>
-            <div className="flex-1">
-              <input
-                type="date"
-                id="date"
-                name="date"
-                value={formData.date}
-                onChange={handleInputChange}
-                className="border  w-full rounded py-2 text-white-700 leading-tight bg-transparent focus:outline-none focus:shadow-outline"
-              />
-            </div>
-          </div>
-          <label className={`block mt-3 text-sm font-medium text-white `}>
+          
+        <div className="mt-4">
+          <input
+            type="text"
+            id="title"
+            name="title"
+            placeholder="Title"
+            value={formData.title}
+            onChange={handleInputChange}
+            className={`border w-full rounded-xl pl-[7px] py-2 text-white-700 leading-tight ${
+              isTitleValid ? "bg-slate-800" : "bg-red-200"
+            } focus:outline-none focus:shadow-outline`}
+//             className="border w-full rounded-xl py-2 text-white leading-tight bg-transparent focus:outline-none focus:shadow-outline" -->
+          />
+          <p className="text-sm text-gray-500">
+            {wordCount(formData.title)} / 25 words
+          </p>
+        </div>
+        <div className="mt-4 mb-4 ">
+          <input
+            type="text"
+            id="slug"
+            name="slug"
+            placeholder="Slug"
+            value={formData.slug}
+            readOnly
+            className="border w-full rounded-xl pl-[7px] py-2 text-white-700 leading-tight bg-slate-800 focus:outline-none focus:shadow-outline"
+//<!--             onChange={handleInputChange}
+    //        className="border w-full rounded-xl py-2 text-white leading-tight bg-transparent focus:outline-none focus:shadow-outline" 
+          />
+        </div>
+        {/* <label className={`block mt-3 text-sm font-medium text-white `}>
             Image
-          </label>
+          </label> */}
           <FileDragDrop image={file} setimage={handleImageChange} />
+        <div className="mt-4">
+          <div>
+            <textarea
+              id="description"
+              name="description"
+              placeholder="Description"
+              value={formData.description}
+              onChange={handleInputChange}
+              className={`border w-full rounded-xl pl-[7px] py-2 text-white-700 leading-tight ${
+                isDescriptionValid ? 'bg-slate-800' : 'bg-black-700'
+              } focus:outline-none focus:shadow-outline`}
+//<!--               className="border  w-full rounded-xl py-2 text-white leading-tight bg-transparent focus:outline-none focus:shadow-outline" 
+            />
+                     
+          </div>
+          <p className="m-0 text-sm text-gray-500">
+          {wordCount(formData.description)} / 100 words
+          </p>
+
+        </div>
+
+          <div className="mt-6">
+          <TextEditor></TextEditor>
+        </div>
           <button
             type="submit"
-            className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline"
           >
             Submit
           </button>
         </div>
-        <div className="mt-6">
-          <TextEditor></TextEditor>
-        </div>
+
       </div>
     </form>
   );
@@ -311,9 +319,7 @@ const FileDragDrop: React.FC<FileDragDrop> = ({ image, setimage }) => {
       onDrop={handleDrop}
       onDragOver={handleDragOver}
       onDragLeave={handleDragLeave}
-      className={` sm:text-lg text-base cursor-pointer w-auto min-h-[10rem] flex items-center justify-center flex-col flex-nowrap text-white border-2 border-white border-opacity-[0.35] rounded-lg ${
-        isDragging ? "" : "border-dashed"
-      }`}
+      className={` sm:text-lg bg-slate-800 text-base rounded-xl cursor-pointer w-auto min-h-[10rem] flex items-center justify-center flex-col flex-nowrap text-white border-2 border-white border-opacity-[0.35] rounded-xl-lg `}
     >
       {uploadedFile && (
         <div>You can drop another image to replace the older one</div>
