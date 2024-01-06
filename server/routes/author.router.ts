a7b08e95f020cc8144fc9fe58f564b42cf4576ab
samsonc <samsonc0514@gmail.com>
2024-01-05 20:45:13 -0500

    updated on author route set up and added documentation on db.server

---
+++ server/routes/author.router.ts
@@ -0,0 +1,35 @@
+// import { PrismaClient } from "@prisma/client";
+import express from "express";
+import { Request, Response } from "express";
+// import { body, validationResult } from "express-validator";
+import * as AuthorService from "./author.service";
+
+export const authorRouter = express.Router();
+
+// GET: List of all Authors
+authorRouter.get("/", async (req: Request, res: Response) => {
+  try {
+    const authors = await AuthorService.listAuthors();
+    console.log(authors);
+    return res.status(200).json(authors);
+  } catch (error: any) {
+    return res.status(500).json(error.message);
+  }
+});
+
+// router.put("/:id", async (req: Request, res: Response) => {
+//     const id = req.params.id;
+//     const blog = await prisma.user.create({
+//         // Add id creator
+//       name: req.body.name,
+//       emailAddress: req.body.emailAddress,
+//       password: req.body.password,
+//       posts: req.body.posts,
+//       imageURL: req.body.imageURL,
+//       createdAt: req.body.createdAt,
+//       updatedAt: req.body.updatedAt
+//     });
+//     return res.status(200).json(blog);
+//   });
+
+// module.exports = router;
