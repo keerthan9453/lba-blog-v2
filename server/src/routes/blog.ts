68e764c0f81b9539a8e25b109aa73913e58cb1a7
theo-thanhlam <phatlam.media.vis@gmail.com>
2024-03-04 13:22:14 -0500

    fix: convert all blogs service from src/routes/blog to src/services/blog and update blog router

--- server/src/routes/blog.ts
+++ server/src/routes/blog.ts
@@ -1,85 +1,13 @@
-import { publicProcedure, router } from "../trpc"
-import {
-    createBlog,
-    deleteBlog,
-    getBlogById,
-    getBlogBySlug,
-    getBlogs,
-    getBlogsByAuthorId,
-    updateBlog,
-} from "../lib/data"
-import { TRPCError } from "@trpc/server"
-import z from "zod"
-import { BlogSchema } from "../schema/blog.schema"
+import { router } from "../trpc"
+import * as BlogService from "../services/blog"
+import { AnyRouter } from "@trpc/server"
 
-// export const blogRouter = router({
-//     getAllBlogs: getAllBlogs(),
-//     getBlogById: getById(),
-//     createBlog: createBlog(),
-// })
-
-export const blogRouter = router({
-    getBlogs: publicProcedure.query(() => {
-        return getBlogs()
-    }),
-    getById: publicProcedure
-        .input(z.object({ id: z.string() }))
-        .query(({ input }) => {
-            const blog = getBlogById(input.id)
-            if (!blog) throw new TRPCError({ code: "NOT_FOUND" })
-            return blog
-        }),
-    getBySlug: publicProcedure
-        .input(z.object({ slug: z.string() }))
-        .query(({ input }) => {
-            const blog = getBlogBySlug(input.slug)
-            if (!blog) throw new TRPCError({ code: "NOT_FOUND" })
-            return blog
-        }),
-    getAuthorBlogs: publicProcedure
-        .input(z.object({ id: z.string() }))
-        .query(async (opts) => {
-            const blogs = getBlogsByAuthorId(opts.input.id)
-            if (!blogs) throw new TRPCError({ code: "NOT_FOUND" })
-            return blogs
-        }),
-    createBlog: publicProcedure.input(BlogSchema).mutation(async (opts) => {
-        const createBlogProcess = await createBlog(opts.input)
-        if (!createBlogProcess)
-            throw new TRPCError({
-                code: "INTERNAL_SERVER_ERROR",
-                message: "Something went wrong",
-            })
-        return createBlogProcess
-    }),
-    updateBlog: publicProcedure
-        .input(
-            z.object({
-                id: z.string(),
-                BlogSchema,
-            }),
-        )
-        .mutation(async (opts) => {
-            const updateBlogProcess = await updateBlog(
-                opts.input.id,
-                opts.input.BlogSchema,
-            )
-            if (!updateBlogProcess)
-                throw new TRPCError({
-                    code: "INTERNAL_SERVER_ERROR",
-                    message: "Something went wrong",
-                })
-            return updateBlogProcess
-        }),
-    deleteBlog: publicProcedure
-        .input(z.object({ id: z.string() }))
-        .mutation(async (opts) => {
-            const detleteBlogProcess = await deleteBlog(opts.input.id)
-            if (!detleteBlogProcess)
-                throw new TRPCError({
-                    code: "INTERNAL_SERVER_ERROR",
-                    message: "Something went wrong",
-                })
-            return detleteBlogProcess
-        }),
+export const blogRouter: AnyRouter = router({
+    getBlogs: BlogService.getAllBlogs(),
+    getById: BlogService.getBlogById(),
+    getBySlug: BlogService.getBlogBySlug(),
+    getAuthorBlogs: BlogService.getBlogsByAuthorId(),
+    createBlog: BlogService.createBlog(),
+    updateBlog: BlogService.updateBlog(),
+    deleteBlog: BlogService.deleteBlog(),
 })
--- server/src/services/blog.ts
+++ server/src/services/blog.ts
@@ -1,32 +1,91 @@
 import { publicProcedure } from "../trpc"
 import { z } from "zod"
 import { BlogSchema } from "../schema/blog.schema"
-import { createBlog, getBlogById, getBlogs } from "../lib/data"
-import { TRPCError } from "@trpc/server"
+import * as dbFunction from "../lib/data"
+import { AnyProcedure, TRPCError } from "@trpc/server"
 
-/*
-TODO: assign type for all of this functions
-*/
+export function getAllBlogs(): AnyProcedure {
+    return publicProcedure.query(() => {
+        return dbFunction.getBlogs()
+    })
+}
 
-// export function getAllBlogs(): any {
-//     publicProcedure.query(() => {
-//         return getBlogs()
-//     })
-// }
+export function getBlogById(): AnyProcedure {
+    return publicProcedure
+        .input(z.object({ id: z.string() }))
+        .query(({ input }) => {
+            const post = dbFunction.getBlogById(input.id)
+            if (!post) throw new TRPCError({ code: "NOT_FOUND" })
+            return post
+        })
+}
 
-// export function getById(): any {
-//     publicProcedure.input(z.object({ id: z.string() })).query(({ input }) => {
-//         const post = getBlogById(input.id)
-//         if (!post) throw new TRPCError({ code: "NOT_FOUND" })
-//         return post
-//     })
-// }
+export function getBlogBySlug(): AnyProcedure {
+    return publicProcedure
+        .input(z.object({ slug: z.string() }))
+        .query(({ input }) => {
+            const blog = dbFunction.getBlogBySlug(input.slug)
+            if (!blog) throw new TRPCError({ code: "NOT_FOUND" })
+            return blog
+        })
+}
 
-// export function createBlog(): any {
-//     publicProcedure.input(BlogSchema).mutation(async (opts) => {
-//         const createBlogProcess = await createBlog(opts.input)
-//         if (!createBlogProcess)
-//             throw new TRPCError({ code: "INTERNAL_SERVER_ERROR" })
-//         return
-//     })
-// }
+export function getBlogsByAuthorId(): AnyProcedure {
+    return publicProcedure
+        .input(z.object({ id: z.string() }))
+        .query(async (opts) => {
+            const blogs = dbFunction.getBlogsByAuthorId(opts.input.id)
+            if (!blogs) throw new TRPCError({ code: "NOT_FOUND" })
+            return blogs
+        })
+}
+
+export function createBlog(): AnyProcedure {
+    return publicProcedure.input(BlogSchema).mutation(async (opts) => {
+        const createBlogProcess = await dbFunction.createBlog(opts.input)
+        if (!createBlogProcess)
+            throw new TRPCError({
+                code: "INTERNAL_SERVER_ERROR",
+                message: "Something went wrong",
+            })
+        return createBlogProcess
+    })
+}
+
+export function updateBlog(): AnyProcedure {
+    return publicProcedure
+        .input(
+            z.object({
+                id: z.string(),
+                BlogSchema,
+            }),
+        )
+        .mutation(async (opts) => {
+            const updateBlogProcess = await dbFunction.updateBlog(
+                opts.input.id,
+                opts.input.BlogSchema,
+            )
+            if (!updateBlogProcess)
+                throw new TRPCError({
+                    code: "INTERNAL_SERVER_ERROR",
+                    message: "Something went wrong",
+                })
+            return updateBlogProcess
+        })
+}
+
+export function deleteBlog(): AnyProcedure {
+    return publicProcedure
+        .input(z.object({ id: z.string() }))
+        .mutation(async (opts) => {
+            const detleteBlogProcess = await dbFunction.deleteBlog(
+                opts.input.id,
+            )
+            if (!detleteBlogProcess)
+                throw new TRPCError({
+                    code: "INTERNAL_SERVER_ERROR",
+                    message: "Something went wrong",
+                })
+            return detleteBlogProcess
+        })
+}
