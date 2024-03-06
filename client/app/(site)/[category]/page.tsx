a05a2557e45ac741ff4e4c21177252dc2a3114eb
DWKim67 <dkim67@my.yorku.ca>
2024-03-05 20:07:32 -0500

    Set up small blog card for content, set up further adjustment with mapping

--- client/app/(site)/[category]/page.tsx
+++ client/app/(site)/[category]/page.tsx
@@ -1,25 +1,68 @@
 "use client";
-
 import SmallBlogCard from "../components/SmallBlogCard";
+
+const defaultCategoryTitle = "AI / Machine Learning";
+const defaultCategoryDescription =
+  "With the rise of tools such as ChatGPT, AI and Machine Learning has \
+crossed over Blockchain technology. Lorem ipsum dolor sit amet, \
+consectetur adipiscing elit, sed do eiusmod tempor incididunt ut \
+labore et dolore magna aliqua.";
+
+interface SmallBlog {
+  title: string;
+  date: string;
+  author: string;
+  content: string;
+}
+
+const exampleBlog: SmallBlog = {
+  title: "Title of Article Goes Here",
+  date: "Date Goes Here",
+  author: "Author Goes Here",
+  content:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \
+  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim \
+  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea \
+  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate \
+  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint \
+  occaecat cupidatat non proident, sunt in culpa qui officia deserunt \
+  mollit anim id est laborum.",
+};
+
+var exampleBlogs = new Array(10);
+exampleBlogs.fill(exampleBlog);
 
 export default function CategoryPage() {
   return (
     <div className="h-full w-full p-16 pt-20">
-      <div className="mt-10 w-8/12">
-        <h1 className="text-6xl font-semibold">AI / Machine Learning</h1>
-        <p className="m-2 mt-5 w-6/12">
-          With the rise of tools such as ChatGPT, AI and Machine Learning has
-          crossed over Blockchain technology. Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua.{" "}
-        </p>
+      <div className="my-32 w-8/12">
+        <h1 className="text-6xl font-semibold">{defaultCategoryTitle}</h1>
+        <p className="m-2 mt-5 w-6/12">{defaultCategoryDescription}</p>
       </div>
-      <div className="mt-28 grid grid-cols-3 gap-4 justify-center items-center">
-        <SmallBlogCard />
-        <SmallBlogCard />
-        <SmallBlogCard />
-        <SmallBlogCard />
+      <div className="grid grid-cols-3 gap-4 justify-center items-center">
+        {exampleBlogs.map((blog, index) => Blog(blog, index))}
       </div>
+    </div>
+  );
+}
+
+function Blog(blog: SmallBlog, index: number) {
+  var blogClassName: string;
+  if (index % 3 == 0) {
+    blogClassName = "justify-start";
+  } else if (index % 3 == 1) {
+    blogClassName = "justify-center";
+  } else {
+    blogClassName = "justify-end";
+  }
+  return (
+    <div className={`flex ${blogClassName}`}>
+      <SmallBlogCard
+        title={blog.title}
+        date={blog.date}
+        author={blog.author}
+        content={blog.content}
+      />
     </div>
   );
 }
--- client/app/(site)/components/SmallBlogCard.tsx
+++ client/app/(site)/components/SmallBlogCard.tsx
@@ -1,29 +1,42 @@
 "use client";
 
-export default function SmallBlogCard() {
+interface SmallBlogProps {
+  title?: string;
+  date?: string;
+  author?: string;
+  content?: string;
+}
+
+const defaultContent =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim \
+veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea \
+commodo consequat. Duis aute irure dolor in reprehenderit in voluptate \
+velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint \
+occaecat cupidatat non proident, sunt in culpa qui officia deserunt \
+mollit anim id est laborum.";
+
+export default function SmallBlogCard({
+  title = "Title of Article Goes Here",
+  date = "Date Goes Here",
+  author = "Author Goes Here",
+  content = defaultContent,
+}: SmallBlogProps) {
   return (
-    <div className="flex flex-col w-80 mr-4 my-2">
+    <div className="flex flex-col w-96 my-2">
       <div className=" bg-slate-500  aspect-video" />
       {/* Replace with IMG later */}
-      <h1 className="mt-2 text-lg font-semibold">Title of Article Goes Here</h1>
-      <h2 className="mt-1 text-sm line-clamp-4">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </h2>
+      <h1 className="mt-2 text-lg font-semibold">{title}</h1>
+      <h2 className="mt-1 text-sm line-clamp-4">{content}</h2>
       <div className="flex mt-2 items-center">
         <img
           src="/avatar.jpg"
           alt="/logo.png"
           className="h-6 w-6 rounded-full"
         />
-        <h2 className="text-xs">Author Goes Here</h2>
+        <h2 className="text-xs">{author}</h2>
         <span className="mx-2 h-1 w-1 rounded-full bg-white" />
-        <h2 className="text-xs">Date Goes Here</h2>
+        <h2 className="text-xs">{date}</h2>
       </div>
     </div>
   );
